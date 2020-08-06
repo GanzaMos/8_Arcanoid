2c4a18ca718379ccd165db7cc654b9e0dd34ffbf
GanzaMos <moskalik.ganza@gmail.com>
2020-08-06 19:09:14 +0300

    4 you lose menu with try again and quit buttons

---
+++ Assets/LoseColliderScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseColliderScript : MonoBehaviour
+{
+    [SerializeField] private GameObject loseMenu;
+
+    private void Awake()
+    {
+        loseMenu.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        loseMenu.SetActive(true);
+    }
+}
