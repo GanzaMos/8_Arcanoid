59453371ca65a980d437dfa6348e0fead26a177f
GanzaMos <moskalik.ganza@gmail.com>
2020-08-06 23:39:35 +0300

    6 ball can launch, blocks are destroyble

--- Assets/BollScript.cs
+++ Assets/BollScript.cs
@@ -6,8 +6,11 @@ public class BollScript : MonoBehaviour
 {
 
     [SerializeField] private GameObject paddle;
+    [SerializeField] private float startPushForce;
     
     private Vector2 posPuddleToBall;
+    private bool isStarted = false;
+    
     void Start()
     {
         posPuddleToBall = transform.position - paddle.transform.position;
@@ -15,7 +18,25 @@ public class BollScript : MonoBehaviour
     
     void Update()
     {
-        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
-        transform.position = paddlePos + posPuddleToBall;
+        LookBalltoPaddle();
+        LaunchBallOnMouseClick();
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, startPushForce);
+            isStarted = true;
+        }
+    }
+
+    private void LookBalltoPaddle()
+    {
+        if (!isStarted)
+        {
+            Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+            transform.position = paddlePos + posPuddleToBall;
+        }
     }
 }
