b0fb61d025f4b923ffd1490bf04fb9e1d046665c
GanzaMos <moskalik.ganza@gmail.com>
2020-08-06 22:56:11 +0300

    5 ball stuck to paddle at the start

---
+++ Assets/BollScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BollScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject paddle;
+    
+    private Vector2 posPuddleToBall;
+    void Start()
+    {
+        posPuddleToBall = transform.position - paddle.transform.position;
+    }
+    
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + posPuddleToBall;
+    }
+}
---
+++ Assets/PaddleScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    [SerializeField] private int widthInUnityLenght = 16;
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
+
+    void Update()
+    {
+        PaddleMoving();
+    }
+
+    private void PaddleMoving()
+    {
+        var paddlePos = Input.mousePosition.x / Screen.width * widthInUnityLenght;
+        paddlePos = Mathf.Clamp(paddlePos, minX, maxX);
+        transform.position = new Vector2(paddlePos, 0.4f);
+    }
+}
