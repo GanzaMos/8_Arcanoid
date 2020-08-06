fcbb9dfe639e0c8b472545137a75cbca92ee2a64
GanzaMos <moskalik.ganza@gmail.com>
2020-08-06 16:23:18 +0300

    2 script for buttons

---
+++ Assets/NextLevelHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelHandler : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        var currentscene = SceneManager.GetActiveScene().buildIndex;
+        
+        if (currentscene + 1 != SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(currentscene + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+}
