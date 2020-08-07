1d93a3234cd34f12e0e41b9863379ae91c0549fb
GanzaMos <moskalik.ganza@gmail.com>
2020-08-07 09:05:16 +0300

    7 prefab several elements + SnapToGrid script for blocks

---
+++ Assets/DeveloperScriptForBlocks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DeveloperScriptForBlocks : MonoBehaviour
+
+{ 
+    void Update()
+    {
+        SnapToGrid();
+    }
+
+    private void SnapToGrid()
+    {
+        transform.position = new Vector2(
+            0.5f + Mathf.RoundToInt(transform.position.x),
+            0.5f + Mathf.RoundToInt(transform.position.y)
+        );
+    }
+}
