d4db6167267ce6d553b2aced96a84bbcdbd016d0
GanzaMos <moskalik.ganza@gmail.com>
2020-08-08 10:54:33 +0300

    13 different breakng levels for blocks

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -7,28 +7,49 @@ public class BlockScript : MonoBehaviour
 {
     private LevelMenuScript winScript;
     private ScoreScript _scoreScript;
+    private SpriteRenderer _spriteRenderer;
 
     [SerializeField] private int scoreAmount = 10;
+    [SerializeField] private int maxHit = 3;
     [SerializeField] private AudioClip destroyAudio;
     [SerializeField] private ParticleSystem deathFX;
     [SerializeField] public BlockType _blockType;
+    [SerializeField] private Sprite[] _sprites;
+
+    private int currentHit = 1;
 
 private void Start()
     {
         winScript = FindObjectOfType<LevelMenuScript>();
         _scoreScript = FindObjectOfType<ScoreScript>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (_blockType == BlockType.Breakeble)
+        if (_blockType == BlockType.Breakeble && currentHit >= maxHit)
         {
-            winScript.DecreaseBlockNumber();
-            _scoreScript.AddScore(scoreAmount);
-            AudioSource.PlayClipAtPoint(destroyAudio, transform.position);
-            DeathParticles();
-            Destroy(gameObject);
+            DestroyBlock();
         }
+        else if (_blockType == BlockType.Breakeble)
+        {
+            HitBlock();
+        }
+    }
+
+    private void HitBlock()
+    {
+        currentHit++;
+        _spriteRenderer.sprite = _sprites[currentHit-1];
+    }
+
+    private void DestroyBlock()
+    {
+        winScript.DecreaseBlockNumber();
+        _scoreScript.AddScore(scoreAmount);
+        AudioSource.PlayClipAtPoint(destroyAudio, transform.position);
+        DeathParticles();
+        Destroy(gameObject);
     }
 
     private void DeathParticles()
